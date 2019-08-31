8a7ef9aaad5b4666cbf3dd8cf78e443a7a237f20
DustedHam <jonathan.gardner@saltboxgames.com>
2019-08-31 10:44:04 -0300

    added simple Json object saving and loading

---
+++ Storage/DataStorage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Saltbox.Storage
+{
+    public interface DataStorage
+    {
+        bool Load<T>(string fileName, out T obj);
+        bool Save<T>(string fileName, T obj);
+    }
+}
+
+
---
+++ Storage/JsonStorage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Saltbox.Storage
+{
+    public class JsonStorage : DataStorage
+    {
+        public bool Load<T>(string name, out T obj)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(Path.Combine(Application.persistentDataPath, $"{name}.json")))
+                {
+                    string data = reader.ReadToEnd();
+                    obj = JsonUtility.FromJson<T>(data);
+                    if (obj != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Debug.LogError(e);
+            }
+            
+            obj = default(T);
+            return false;
+        }
+
+        public bool Save<T>(string name, T obj)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(Path.Combine(Application.persistentDataPath, $"{name}.json"), false))
+                {
+                    string data = JsonUtility.ToJson(obj);
+                    writer.Write(data);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
+            return false;
+        }
+    }
+}
