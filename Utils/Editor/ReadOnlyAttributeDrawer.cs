e70431eea08afc3c22df72b422b71ff134bb3639
DustedHam <jonathan.gardner@saltboxgames.com>
2019-08-31 15:56:16 -0300

    Added license header

--- Utils/Editor/ReadOnlyAttributeDrawer.cs
+++ Utils/Editor/ReadOnlyAttributeDrawer.cs
@@ -1,4 +1,9 @@
-﻿using UnityEditor;
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using UnityEditor;
+
 using UnityEngine;
 
 namespace Saltbox.Utils.Editor
